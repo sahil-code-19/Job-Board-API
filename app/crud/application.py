1f746cca08531c3214ff17468da5835b2344f95a
sahil-code-19 <sahil@softcolon.com>
2026-05-18 09:33:27 +0530

    feat(email): add email functionality for confirmation of job applied! - Add email functionality to POST /api/application/apply endpoint

--- app/crud/application.py
+++ app/crud/application.py
@@ -5,8 +5,8 @@ from typing import List
 
 from ..models import Application
 
-async def apply_job(db: AsyncSession, resume_path: str, user_id: int, job_id: int, status: str, cover_letter: str):
-    application = Application(candidate_id= user_id, job_id= job_id, status= status, resume_path=resume_path, cover_letter= cover_letter)
+async def apply_job(db: AsyncSession, resume_path: str, user_id: int, job_id: int, cover_letter: str):
+    application = Application(candidate_id= user_id, job_id= job_id, resume_path=resume_path, cover_letter= cover_letter)
     db.add(application)
     await db.commit()
     await db.refresh(application)
